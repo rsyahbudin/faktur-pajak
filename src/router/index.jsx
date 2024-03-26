584858d244fa28746a4c7044f5f76e0d177f7661
rsyahbudin <syahbudin45@gmail.com>
2024-03-26 17:16:28 +0700

    [ADD] Error Page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,14 +3,14 @@ import { useRoutes } from "react-router-dom";
 import Homepage from "../pages/Homepage";
 import StatusRequest from "../pages/StatusRequest";
 import Admin from "../pages/admin"
+import Page404 from "../pages/404";
 
 export default function Routes() {
   const element = useRoutes([
     { path: "/", element: <Homepage /> },
     { path: "/status", element: <StatusRequest /> },
     { path: "/admin", element: <Admin /> },
-    
-    // { path: "*", element: <Page404 /> },
+    { path: "*", element: <Page404 /> },
   ]);
   return element;
 }
